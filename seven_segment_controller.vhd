71947bfdfa3a538e090a4c330dbf3c6be03f2951
Andrey Reznikov <andreyrez02@gmail.com>
2020-10-16 14:29:58 +0300

    Небольшие исправления
    Перепелицину так больше нравится

--- seven_segment_controller.vhd
+++ seven_segment_controller.vhd
@@ -6,7 +6,7 @@ entity seven_segment_controller is
 	port(
 		ready_in     : in  std_logic;
 		clk          : in  std_logic;
-		indicator_in : in  std_logic_vector(0 to 7);
+		indicator_in : in  std_logic_vector(7 downto 0);
 		data_out     : out std_logic;
 		sck          : out std_logic;
 		ready_out    : out std_logic

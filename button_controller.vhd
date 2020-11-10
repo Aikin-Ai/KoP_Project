9d62b9749a914722bcfa6dc7f6e96870b9e2b41d
Andrey Reznikov <andreyrez02@gmail.com>
2020-11-10 18:12:55 +0200

    Полностью переделал button_controller

--- button_controller.vhd
+++ button_controller.vhd
@@ -1,8 +1,12 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
 
 entity button_controller is
-
+	generic(
+		clk_freq : integer := 1_000;
+		stable_time : integer := 10
+	);
 	port(
 		clk        : in  std_logic;
 		button_in  : in  std_logic;
@@ -12,23 +16,23 @@ entity button_controller is
 end button_controller;
 
 architecture rtl of button_controller is
-
+	signal flipflops : std_logic_vector(1 downto 0);
+	signal counter_set : std_logic;
 begin
-	strobe : process(clk) is
-		variable counter : integer := 0;
+	counter_set <= flipflops(0) xor flipflops(1);
+	process(clk)
+		variable count : integer range 0 to clk_freq*stable_time/1000;
 	begin
-		if (rising_edge(clk)) then
-			if (counter > 0) then
-				button_out <= '0';
-				if (counter < 10) then
-					counter := counter + 1;
-				else
-					counter := 0;
-				end if;
-			elsif (counter = 0 and button_in = '1') then
-				counter    := 1;
-				button_out <= '1';
+		if(clk'event and clk = '1') then
+			flipflops(0) <= button_in;
+			flipflops(1) <= flipflops(0);
+			if(counter_set = '1') then
+				count := 0;
+			elsif(count < clk_freq*stable_time/1000) then
+				count := count + 1;
+			else
+				button_out <= flipflops(1);
 			end if;
 		end if;
-	end process strobe;
+	end process;
 end rtl;
